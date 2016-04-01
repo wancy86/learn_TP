d204eef8b338ba662e33e240242134d69980475c
Mark <wancy86@sina.com>
2016-04-01 22:24:59 +0800

    gitignore

--- Home/Runtime/Temp/caf9b6b99962bf5c2264824231d7a40c.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000003600a:13:{s:13:"THINK_VERSION";s:5:"3.1.3";s:15:"SERVER_SOFTWARE";s:48:"Apache/2.4.10 (Win32) OpenSSL/0.9.8zb PHP/5.2.17";s:6:"PHP_OS";s:5:"WINNT";s:5:"mysql";s:6:"5.5.40";s:4:"core";a:6:{s:15:"UPLOAD_CAPACITY";s:3:"100";s:20:"UPLOAD_USER_CAPACITY";s:2:"50";s:20:"UPLOAD_FILE_PIC_SIZE";s:4:"1024";s:20:"UPLOAD_FILE_PIC_TYPE";s:15:"jpg|gif|png|bmp";s:21:"UPLOAD_FILE_FILE_SIZE";s:11:"10000000000";s:21:"UPLOAD_FILE_FILE_TYPE";s:38:"rar|zip|txt|doc|docx|ppt|pptx|xls|xlsx";}s:4:"user";s:1:"3";s:4:"file";s:1:"1";s:4:"news";s:2:"11";s:11:"clientcount";s:1:"4";s:12:"clientcount2";s:1:"2";s:12:"clientcount3";s:1:"2";s:12:"clientcount4";s:1:"0";s:12:"contactcount";s:1:"5";}
-?>
